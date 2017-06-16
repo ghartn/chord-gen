2012930bf4298f7831e3c265fd732e65a64648c1
Garrett <garrett.hartnell@fuseforward.com>
2017-06-16 14:23:43 -0700

    Init commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('/api/watson/tone', (req, res) => {
+	console.log(req);
+	res.send("we gettin jiggy with react and express and a proxy skrr")
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
