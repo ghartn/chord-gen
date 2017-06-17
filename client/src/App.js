7ce25ce598c0174a19d801edc3e0c98fa9250c09
garrett <garrett.hartnell@gmail.com>
2017-06-17 13:34:28 -0700

    Working tone anaylyzer api - now to generate some chords!

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import { Input, Header, Button } from 'semantic-ui-react'
+import {Input, Header, Button} from 'semantic-ui-react';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super()
-    this.onClick = this.onClick.bind(this);
+    this.onClick = this
+      .onClick
+      .bind(this);
+    this.onChange = this
+      .onChange
+      .bind(this);
   }
 
   onClick() {
+    axios
+      .post('/api/watson/tone', this.state)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
-        <div className="App">
-          <div className="App-vc">
-            <Header size='large'>How do you feel?</Header>
-            <Input placeholder='...' />
-            <Button primary onClick={this.onClick}>Generate</Button>
-          </div>
+      <div className="App">
+        <div className="App-vc">
+          <Header size='large'>How do you feel?</Header>
+          <Input name="feel" placeholder='...' onChange={this.onChange}/>
+          <Button primary onClick={this.onClick}>Generate</Button>
         </div>
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express');
-
+var bodyParser = require('body-parser')
 const app = express();
 
 app.set('port', (process.env.PORT || 3001));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
+var tone_analyzer = new ToneAnalyzerV3({url: "https://gateway.watsonplatform.net/tone-analyzer/api", username: "7f240490-e244-45a2-90b4-8a3e11f13089", password: "6iJSZ7uIBYk5", version_date: '2016-05-19'});
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.get('/api/watson/tone', (req, res) => {
-	console.log(req);
-	res.send("we gettin jiggy with react and express and a proxy skrr")
-});
+app
+  .post('/api/watson/tone', function (req, res, next) {
+    var feel = req.body.feel;
+    tone_analyzer.tone({
+      text: feel
+    }, function (err, tone) {
+      if (err) 
+        console.log(err);
+      else 
+        console.log(JSON.stringify(tone, null, 2));
+      }
+    );
+  });
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
