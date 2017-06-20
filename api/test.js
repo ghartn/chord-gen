725a77d5a07102a933b45f4ada10f69a250735c4
garrett hartnell <garrett.hartnell@gmail.com>
2017-06-20 10:23:49 -0700

    Too many scales perhaps cluttering/overcomplicating things, maybe just need major/minor for now!

--- api/test.js
+++ api/test.js
@@ -2,97 +2,136 @@ var scales = require("./scales");
 
 function main() {
 	let tone = {
-		document_tone: {
-			tone_categories: [
-				{
-					tones: [
-						{
-							score: 0.088655,
-							tone_id: "anger",
-							tone_name: "Anger"
-						},
-						{
-							score: 0.073668,
-							tone_id: "disgust",
-							tone_name: "Disgust"
-						},
-						{
-							score: 0.179802,
-							tone_id: "fear",
-							tone_name: "Fear"
-						},
-						{
-							score: 0.276571,
-							tone_id: "joy",
-							tone_name: "Joy"
-						},
-						{
-							score: 0.325982,
-							tone_id: "sadness",
-							tone_name: "Sadness"
-						}
-					],
-					category_id: "emotion_tone",
-					category_name: "Emotion Tone"
-				},
-				{
-					tones: [
-						{
-							score: 0.620279,
-							tone_id: "analytical",
-							tone_name: "Analytical"
-						},
-						{
-							score: 0,
-							tone_id: "confident",
-							tone_name: "Confident"
-						},
-						{
-							score: 0.994446,
-							tone_id: "tentative",
-							tone_name: "Tentative"
-						}
-					],
-					category_id: "language_tone",
-					category_name: "Language Tone"
-				},
-				{
-					tones: [
-						{
-							score: 0.284673,
-							tone_id: "openness_big5",
-							tone_name: "Openness"
-						},
-						{
-							score: 0.153895,
-							tone_id: "conscientiousness_big5",
-							tone_name: "Conscientiousness"
-						},
-						{
-							score: 0.103424,
-							tone_id: "extraversion_big5",
-							tone_name: "Extraversion"
-						},
-						{
-							score: 0.894485,
-							tone_id: "agreeableness_big5",
-							tone_name: "Agreeableness"
-						},
-						{
-							score: 0.005693,
-							tone_id: "emotional_range_big5",
-							tone_name: "Emotional Range"
-						}
-					],
-					category_id: "social_tone",
-					category_name: "Social Tone"
-				}
-			]
-		}
-	};
+  "document_tone": {
+    "tone_categories": [
+      {
+        "tones": [
+          {
+            "score": 0.019665,
+            "tone_id": "anger",
+            "tone_name": "Anger"
+          },
+          {
+            "score": 0.014064,
+            "tone_id": "disgust",
+            "tone_name": "Disgust"
+          },
+          {
+            "score": 0.554403,
+            "tone_id": "fear",
+            "tone_name": "Fear"
+          },
+          {
+            "score": 0.007789,
+            "tone_id": "joy",
+            "tone_name": "Joy"
+          },
+          {
+            "score": 0.956565,
+            "tone_id": "sadness",
+            "tone_name": "Sadness"
+          }
+        ],
+        "category_id": "emotion_tone",
+        "category_name": "Emotion Tone"
+      },
+      {
+        "tones": [
+          {
+            "score": 0.970179,
+            "tone_id": "analytical",
+            "tone_name": "Analytical"
+          },
+          {
+            "score": 0,
+            "tone_id": "confident",
+            "tone_name": "Confident"
+          },
+          {
+            "score": 0,
+            "tone_id": "tentative",
+            "tone_name": "Tentative"
+          }
+        ],
+        "category_id": "language_tone",
+        "category_name": "Language Tone"
+      },
+      {
+        "tones": [
+          {
+            "score": 0.220893,
+            "tone_id": "openness_big5",
+            "tone_name": "Openness"
+          },
+          {
+            "score": 0.273139,
+            "tone_id": "conscientiousness_big5",
+            "tone_name": "Conscientiousness"
+          },
+          {
+            "score": 0.539658,
+            "tone_id": "extraversion_big5",
+            "tone_name": "Extraversion"
+          },
+          {
+            "score": 0.599812,
+            "tone_id": "agreeableness_big5",
+            "tone_name": "Agreeableness"
+          },
+          {
+            "score": 0.086649,
+            "tone_id": "emotional_range_big5",
+            "tone_name": "Emotional Range"
+          }
+        ],
+        "category_id": "social_tone",
+        "category_name": "Social Tone"
+      }
+    ]
+  }
+}
 	let tonePoint = cleanTone(tone);
 	let generatedScale = determineScale(tonePoint);
 	console.log(generatedScale);
+}
+
+function euclideanDistance(point1, point2) {
+	if (point1.length != point2.length) return -1;
+	var euclideanDistance = 0;
+	for (var i = 0; i < point1.length; i++) {
+		euclideanDistance += Math.pow(point1[i] - point2[i], 2);
+	}
+	euclideanDistance = Math.sqrt(euclideanDistance);
+	return euclideanDistance;
+}
+
+function cleanTone(tone) {
+	let point = [];
+	tone = tone.document_tone;
+	for (var key in tone.tone_categories) {
+		let toneCategory = tone.tone_categories[key];
+		let tones = toneCategory.tones;
+		for (var key in tones) {
+			point.push(tones[key].score);
+		}
+	}
+	console.log(point)
+	return point;
+}
+
+function determineScale(tonePoint) {
+	var distances = [];
+	for (var index in scales) {
+		let scale = scales[index];
+		//overly convoluted way of getting the object
+		let scalePoint = scales[index][Object.keys(scales[index])[0]];
+		distances.push(euclideanDistance(tonePoint, scalePoint));
+	}
+	let minDistance = Math.min(...distances);
+	let scaleIndex = distances.indexOf(minDistance);
+	let determinedScale = scales[scaleIndex];
+	let scale = Object.keys(determinedScale)[0];
+	return scale;
 }
 
 
