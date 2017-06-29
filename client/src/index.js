a7a71e4948deb346609f06a4259198d4c2f39e9f
garrett hartnell <garrett.hartnell@gmail.com>
2017-06-29 16:52:32 -0700

    quick and untested react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, browserHistory } from "react-router";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+ReactDOM.render(
+	<Router history={browserHistory}>
+		<Route path="/" component={App} />
+	</Router>,
+	document.getElementById("root")
+);
 registerServiceWorker();
